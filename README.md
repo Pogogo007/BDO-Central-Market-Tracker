0b9a3d0f211c48447fb74a0c59a90104e9881782
Pogogo007 <Francisd246@hotmail.com>
2021-03-22 17:08:10 -0400

    reupload

---
+++ README.md
@@ -0,0 +1,29 @@
+# Central-Market-Tracker
+A BDO tool that allows you to monitor trades for central marketplace items.
+Was intended to be used in conjunction with bdowhaletracker.com but i now know that it may have more applications.
+Keep In Mind That PA may rate limit you or temp ban you from accessing the website if you do too many requests in a short period of time.
+
+## Setup
+# Download the program from the releases tab. Make Sure to Extract The Folder
+
+I Took this guide directly from https://github.com/kookehs/bdo-marketplace as it is the same concept
+
+You can use the developer tools provided by your browser to look at network requests for the site.
+
+Requires the two `__RequestVerificationToken`
+
+1. https://na-trade.naeu.playblackdesert.com/Intro/ or https://eu-trade.naeu.playblackdesert.com/Intro/
+    - Select your region and sign in.
+2. Open `Developer Tools` for your browser and select `Network`.
+    - For Chrome it's `Ctrl+Shift+I` and click `Network` tab.
+    - For Firefox it's `Ctrl+Shift+E` to open `Network` tab.
+3. Click the search icon. Search for any item.
+4. Click on the listing to open up details for that item.
+5. Check for a request to `GetItemSellBuyInfo` in `Network` tab.
+    - `headerToken` is found in `Request Headers` under `Cookie` -> `__RequestVerificationToken`. Do NOT include the = and ; at the end of this token.
+    - `formToken` is found in `Form Data` under `__RequestVerificationToken`.
+6. Copy those two Tokens and set them in settings.yaml
+7. Set your region in the settings. EU or NA only.
+8. Get the ids for the item you want to track and place them in the settings in the correct format.I have put examples ids in the settings already. BDOCodex has the IDS for most      items in the game. You will find it in the address bar
+9. Modify delay between checks. In seconds. `DO NOT PUT THIS TOO LOW.` Otherwise the program may not be able to do its checks correctly and PA may block your account from accessing the website temporarely. 30 Seconds is fine for most people
+10. Run the EXE
---
+++ src/main.py
@@ -0,0 +1,110 @@
+from pprint import pprint
+import json
+import requests
+import time
+import yaml
+import sys
+
+pprint("Starting Up")
+startup = True
+config = yaml.safe_load(open('settings.yaml', 'r'))
+itemIDS = config['itemids']
+currentTrades = dict()
+timer = config["delay"]
+region = config['region']
+headerToken = config['headerToken']
+formToken = config['formToken']
+userAgent = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/89.0.4389.90 Safari/537.36 Edg/89.0.774.54"
+
+if region == 'NA':
+    LINK = "https://na-trade.naeu.playblackdesert.com/Home/GetWorldMarketSubList"
+elif region == 'EU':
+    LINK = "https://eu-trade.naeu.playblackdesert.com/Home/GetWorldMarketSubList"
+else:
+    pprint("Wrong Region. Use EU or NA. Press Enter To Exit")
+    input()
+    exit()
+
+if headerToken == None or formToken == None:
+    pprint("You Have not set your Header Token Or Form Token. Press Enter To Exit")
+    input()
+    exit()
+
+def progressBar(value, endvalue, bar_length=20):
+
+        percent = float(value) / endvalue
+        arrow = '-' * int(round(percent * bar_length)-1) + '>'
+        spaces = ' ' * (bar_length - len(arrow))
+
+        sys.stdout.write("\rPercent: [{0}] {1}%\r".format(arrow + spaces, int(round(percent * 100))))
+        sys.stdout.flush()
+        
+def updateTradeCount():
+    pprint("Checking For New Trades...")
+    i = 0
+    max = len(itemIDS)
+    new = False
+    for id in itemIDS:
+        i = i + 1
+        progressBar(i, max)
+        global currentTrades
+        currentIDTr = currentTrades[id]
+        cookies = {"__RequestVerificationToken": headerToken}
+        headers = {'User-Agent': userAgent, 'Content-Type': 'application/x-www-form-urlencoded; charset=UTF-8'}
+        data = {"__RequestVerificationToken": formToken, "mainKey": id}
+        page = requests.post(LINK, headers=headers, data=data, cookies=cookies)
+        info = json.loads(page.content)
+        content = info.get('detailList')
+        if content == []:
+            pprint("Something Went Horribly Wrong or the server did not answer. Exiting")
+            exit()
+        else:
+            details = content[0]
+            newTrades = details['totalTradeCount']
+            if newTrades == currentIDTr:
+                #pprint("No New Trades Detected For {} Within the last {} seconds".format(details['name'], timer))
+                time.sleep(0.5)
+            else:
+                difference = newTrades - currentIDTr
+                currentTrades[id] = newTrades
+                pprint("{} new trades detected for {} within the last {} seconds! New Amount Of Total Trades at {}".format(difference, details['name'], timer, newTrades))
+                #pprint("New Amount Of Total Trades at {}".format(newTrades))
+                new = True
+                time.sleep(0.5)
+    if(new == False):
+        pprint("No New Trades Found Within The Last {} Seconds".format(timer))
+        
+        
+for id in itemIDS:
+    currentTrades[id] = 0
+
+pprint("Startup Done")
+if startup == True:
+    for id in itemIDS:
+        cookies = {"__RequestVerificationToken": headerToken}
+        headers = {'User-Agent': userAgent, 'Content-Type': 'application/x-www-form-urlencoded; charset=UTF-8'}
+        data = {"__RequestVerificationToken": formToken, "mainKey": id}
+        page = requests.post(LINK, headers=headers, data=data, cookies=cookies)
+        info = json.loads(page.content)
+        content = info.get('detailList')
+        if content == []:
+            pprint("{} Is An Invalid ID. Please Change It. Press Enter to Exit.".format(id))
+            input()
+            exit()
+        else:
+            details = content[0]
+            pprint("Monitoring Started for {}".format(details['name']))
+            pprint("Amount Of Total Trades {}".format(details['totalTradeCount']))
+            currentTrades[id] = details['totalTradeCount']
+            time.sleep(0.5)
+    startup = False
+        
+print("press ctrl-c to stop")
+loop_forever = True
+while loop_forever:
+    updateTradeCount()
+    try:
+        time.sleep(timer)
+    except KeyboardInterrupt:
+        loop_forever = False
+        pprint("Keyboard Interupt Triggered Exiting...")
