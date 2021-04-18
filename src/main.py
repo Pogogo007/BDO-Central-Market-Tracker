9143c51649c110d2c5fdedffdf46e7a45a4ae7b2
Pogogo007 <Francisd246@hotmail.com>
2021-04-18 14:04:09 -0400

    add the ability to track price changes

--- src/main.py
+++ src/main.py
@@ -19,12 +19,18 @@ class colors:
     cyan = "\033[36m"
     white = "\033[37m"
     reset = "\033[0m"
-    
+
+
 old_print = print
+
+
 def timestamped_print(*args, **kwargs):
     timeObj = datetime.now().time()
-    time = ''.join((str(timeObj.hour),':',str(timeObj.minute),':',str(timeObj.second)))
+    time = ''.join((str(timeObj.hour), ':', str(
+        timeObj.minute), ':', str(timeObj.second)))
     old_print(colors.magenta + time + colors.reset, *args, **kwargs)
+
+
 print = timestamped_print
 
 
@@ -32,9 +38,13 @@ print(colors.yellow + "Starting Up" + colors.reset)
 startup = True
 config = yaml.safe_load(open('settings.yaml', 'r'))
 itemIDS = config['itemids']
-currentTrades = dict()
+currentAmount = dict()
+for id in itemIDS:
+    currentAmount[id] = 0
 timer = config["delay"]
 region = config['region']
+mode = config['mode']
+loop_forever = True
 headerToken = config['headerToken']
 formToken = config['formToken']
 userAgent = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/89.0.4389.90 Safari/537.36 Edg/89.0.774.54"
@@ -53,16 +63,19 @@ if headerToken == None or formToken == None:
     input()
     exit()
 
+
 def progressBar(value, endvalue, bar_length=20):
 
-        percent = float(value) / endvalue
-        arrow = '-' * int(round(percent * bar_length)-1) + '>'
-        spaces = ' ' * (bar_length - len(arrow))
+    percent = float(value) / endvalue
+    arrow = '-' * int(round(percent * bar_length)-1) + '>'
+    spaces = ' ' * (bar_length - len(arrow))
 
-        sys.stdout.write(colors.magenta + "\rPercent: [{0}] {1}%\r".format(arrow + spaces, int(round(percent * 100))) + colors.reset)
-        sys.stdout.flush()
-        
-def updateTradeCount():
+    sys.stdout.write(colors.magenta + "\rPercent: [{0}] {1}%\r".format(
+        arrow + spaces, int(round(percent * 100))) + colors.reset)
+    sys.stdout.flush()
+
+
+def updateTrades():
     print(colors.yellow + "Checking For New Trades..." + colors.reset)
     i = 0
     max = len(itemIDS)
@@ -70,10 +83,11 @@ def updateTradeCount():
     for id in itemIDS:
         i = i + 1
         progressBar(i, max)
-        global currentTrades
-        currentIDTr = currentTrades[id]
+        global currentAmount
+        currentIDTr = currentAmount[id]
         cookies = {"__RequestVerificationToken": headerToken}
-        headers = {'User-Agent': userAgent, 'Content-Type': 'application/x-www-form-urlencoded; charset=UTF-8'}
+        headers = {'User-Agent': userAgent,
+                   'Content-Type': 'application/x-www-form-urlencoded; charset=UTF-8'}
         data = {"__RequestVerificationToken": formToken, "mainKey": id}
         page = requests.post(LINK, headers=headers, data=data, cookies=cookies)
         info = json.loads(page.content)
@@ -85,26 +99,60 @@ def updateTradeCount():
             details = content[0]
             newTrades = details['totalTradeCount']
             if newTrades == currentIDTr:
-                #pprint("No New Trades Detected For {} Within the last {} seconds".format(details['name'], timer))
                 time.sleep(0.5)
             else:
                 difference = newTrades - currentIDTr
-                currentTrades[id] = newTrades
-                print(colors.green + "\033[34m{}\033[32m new trades detected for \033[34m{}\033[32m within the last {} seconds! New Amount Of Total Trades at {}".format(difference, details['name'], timer, newTrades) + colors.reset)
+                currentAmount[id] = newTrades
+                print(colors.green + "\033[34m{}\033[32m new trades detected for \033[34m{}\033[32m within the last {} seconds! New Amount Of Total Trades at {}".format(
+                    difference, details['name'], timer, newTrades) + colors.reset)
                 #pprint("New Amount Of Total Trades at {}".format(newTrades))
                 new = True
                 time.sleep(0.5)
     if(new == False):
         print(colors.red + "No New Trades Found Within The Last {} Seconds".format(timer) + colors.reset)
-        
-        
-for id in itemIDS:
-    currentTrades[id] = 0
 
-if startup == True:
+
+def updatePrice():
+    print(colors.yellow + "Checking For Price Changes..." + colors.reset)
+    i = 0
+    max = len(itemIDS)
+    new = False
+    for id in itemIDS:
+        i = i + 1
+        progressBar(i, max)
+        global currentAmount
+        currentIDPr = currentAmount[id]
+        cookies = {"__RequestVerificationToken": headerToken}
+        headers = {'User-Agent': userAgent,
+                   'Content-Type': 'application/x-www-form-urlencoded; charset=UTF-8'}
+        data = {"__RequestVerificationToken": formToken, "mainKey": id}
+        page = requests.post(LINK, headers=headers, data=data, cookies=cookies)
+        info = json.loads(page.content)
+        content = info.get('detailList')
+        if content == []:
+            print(colors.red + "Something Went Horribly Wrong or the server did not answer. Exiting" + colors.reset)
+            exit()
+        else:
+            details = content[0]
+            newPrice = details['pricePerOne']
+            if newPrice == currentIDPr:
+                time.sleep(0.5)
+            else:
+                #difference = newPrice - currentIDPr
+                currentAmount[id] = newPrice
+                print(colors.green + "New Price detected for \033[34m{}\033[32m within the last {} seconds! New Price At {}".format(
+                    details['name'], timer, newPrice) + colors.reset)
+                #pprint("New Amount Of Total Trades at {}".format(newTrades))
+                new = True
+                time.sleep(0.5)
+    if(new == False):
+        print(colors.red + "No Price Change Found Within The Last {} Seconds".format(timer) + colors.reset)
+
+def startupTrades():
     for id in itemIDS:
         cookies = {"__RequestVerificationToken": headerToken}
-        headers = {'User-Agent': userAgent, 'Content-Type': 'application/x-www-form-urlencoded; charset=UTF-8'}
+        headers = {'User-Agent': userAgent,
+                   'Content-Type': 'application/x-www-form-urlencoded; charset=UTF-8'}
         data = {"__RequestVerificationToken": formToken, "mainKey": id}
         page = requests.post(LINK, headers=headers, data=data, cookies=cookies)
         info = json.loads(page.content)
@@ -115,20 +163,58 @@ if startup == True:
             exit()
         else:
             details = content[0]
-            print(colors.green + "Monitoring Started for {}".format(details['name']) + colors.reset)
-            print(colors.green + "Amount Of Total Trades {}".format(details['totalTradeCount']) + colors.reset)
-            currentTrades[id] = details['totalTradeCount']
+            print(
+                colors.green + "Monitoring Started for {}".format(details['name']) + colors.reset)
+            print(colors.green + "Amount Of Total Trades {}".format(
+                details['totalTradeCount']) + colors.reset)
+            currentAmount[id] = details['totalTradeCount']
             time.sleep(0.5)
     startup = False
     print(colors.yellow + "Startup Done" + colors.reset)
     
+def startupPrice():
+    for id in itemIDS:
+        cookies = {"__RequestVerificationToken": headerToken}
+        headers = {'User-Agent': userAgent,
+                   'Content-Type': 'application/x-www-form-urlencoded; charset=UTF-8'}
+        data = {"__RequestVerificationToken": formToken, "mainKey": id}
+        page = requests.post(LINK, headers=headers, data=data, cookies=cookies)
+        info = json.loads(page.content)
+        content = info.get('detailList')
+        if content == []:
+            print(colors.red + "{} Is An Invalid ID. Please Change It. Press Enter to Exit.".format(id) + colors.reset)
+            input()
+            exit()
+        else:
+            details = content[0]
+            print(
+                colors.green + "Monitoring Started for {}".format(details['name']) + colors.reset)
+            print(colors.green + "Current Price {}".format(
+                details['pricePerOne']) + colors.reset)
+            currentAmount[id] = details['pricePerOne']
+            time.sleep(0.5)
+    startup = False
+    print(colors.yellow + "Startup Done" + colors.reset)
+
 print(colors.cyan + "Press Ctrl-C to Stop at Any Time" + colors.reset)
 
-loop_forever = True
-while loop_forever:
-    updateTradeCount()
-    try:
-        time.sleep(timer)
-    except KeyboardInterrupt:
-        loop_forever = False
-        print("Keyboard Interupt Triggered Exiting...")
+if startup == True:
+    if(mode == 'trades'):
+        startupTrades()
+        while loop_forever:
+            updateTrades()
+            try:
+                time.sleep(timer)
+            except KeyboardInterrupt:
+                loop_forever = False
+                print("Keyboard Interupt Triggered Exiting...")
+    elif(mode == 'price'):
+        startupPrice()
+        while loop_forever:
+                updatePrice()
+                try:
+                    time.sleep(timer)
+                except KeyboardInterrupt:
+                    loop_forever = False
+                    print("Keyboard Interupt Triggered Exiting...")
+
